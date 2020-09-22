b497b5c1bcf508c3c65b587b34040e9738b65de8
Gudi Varaprasad <53944466+GudiVaraprasad@users.noreply.github.com>
2020-09-22 11:26:50 +0530

    Delete script.js

--- JS_Magical-BG/script.js
+++
@@ -1,5 +0,0 @@
-
-function changecolor(id) {
-	document.body.style.background = document.getElementById(id).innerHTML;
-	document.addEventListener('contextmenu', event => event.preventDefault());
-}
