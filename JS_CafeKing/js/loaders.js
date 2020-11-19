a575bbf2a920daee41195a5e05e6158fce4ec511
Gudi Varaprasad <53944466+GudiVaraprasad@users.noreply.github.com>
2020-11-19 14:02:11 +0530

    Add files via upload

---
+++ JS_CafeKing/js/loaders.js
@@ -0,0 +1,24 @@
+$(document).ready(function () {
+  $(".sidenav").sidenav();
+  $(".slider").slider({ full_width: true });
+
+  $(".myreviews").carousel({
+    numVisible: 7,
+    shift: 55,
+    padding: 55,
+  });
+
+  $(".modal").modal();
+  $(".parallax").parallax();
+
+  $(".carousel.carousel-slider.myslider").carousel({
+    fullWidth: true,
+    indicators: true,
+  });
+});
+
+function toggleModal() {
+  var instance = M.Modal.getInstance($("#modal3"));
+
+  instance.open();
+}
